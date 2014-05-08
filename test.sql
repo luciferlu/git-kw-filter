6fb73cfa1a9c27b51c3eb5f12e2ff6b1e60b5e00
Lucifer Lu <yuntao.lu@gmail.com>
2014-05-08 17:14:18 +0800

    test again

--- test.sql
+++ test.sql
@@ -1,5 +1,6 @@
--- $Revision:ba13c6115dff949a4057d7a7d0a0e9b9bd8d65fe$
--- $Author:Lucifer Lu <yuntao.lu@gmail.com>$
--- $Date:2014-05-08 16:17:14 +0800$
+-- $Revision$
+-- $Author$
+-- $Date$
 
 SELECT * FROM DUAL;
+;
